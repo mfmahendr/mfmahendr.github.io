d42fa1fe4117d2ce67dcadb0342db21854759bd2
M. Fadhil Mahendra <mfmahendr@gmail.com>
2026-01-30 07:49:53 +0700

    feat: :bento: add and replace illustration data and its associated image

---
+++ src/assets/data/illustrationsData.js
@@ -0,0 +1,96 @@
+export const profilePicture = {
+  id: {
+    file: "fadhil.png",
+    attribution: {
+      author: "chenyuee.e @Instagram",
+      source: {
+        label: "Private Commission",
+        url: "null",
+      },
+      license: "Copyright transferred (exclusive rights)",
+    },
+  },
+  en: {
+    file: "fadhil-wearing-batik.png",
+    attribution: {
+      author: "chenyuee.e @Instagram",
+      source: {
+        label: "Private Commission",
+        url: "null",
+      },
+      license: "Copyright transferred (exclusive rights)",
+    },
+  },
+};
+
+export const breakingNewsIllustratin = {
+  opento: {
+    file: "opento.png",
+    attribution: {
+      author: "Jean-Baptiste Madou",
+      source: {
+        label: "Wikimedia Commons",
+        url: "https://commons.wikimedia.org/wiki/File:Le_bon_conseil.jpg",
+      },
+      license: "Public Domain",
+    },
+  },
+  notopento: {
+    file: "notopento.png",
+    attribution: {
+      author: "Jean-Baptiste Madou",
+      source: {
+        label: "Wikimedia Commons",
+        url: "https://commons.wikimedia.org/wiki/File:Le_bon_conseil.jpg",
+      },
+      license: "Public Domain",
+    },
+  },
+};
+
+export const learningIllustrations = [
+  {
+    file: "reading-man-willem-van-nieuwenhoven.png",
+    attribution: {
+      author: "Willem van Nieuwenhoven",
+      source: {
+        label: "Simonis & Buunk Fine Paintings, Ede, The Netherlands,",
+        url: "https://www.simonis-buunk.com/artwork/willem-van-nieuwenhoven-painting-reading-man/13214/",
+      },
+      license: "Public Domain",
+    },
+  },
+  {
+    file: "the-passion-of-creation.png",
+    attribution: {
+      author: "Leonid Pasternak",
+      source: {
+        label: "Wikimedia Commons",
+        url: "https://commons.wikimedia.org/wiki/File:Leonid_Pasternak_-_The_Passion_of_creation_(1).jpg",
+      },
+      license: "Public Domain",
+    },
+  },
+  {
+    file: "reading-a-manuscript-leonid-pasternak.png",
+    attribution: {
+      author: "Leonid Pasternak",
+      source: {
+        label: "THE TRETYAKOV GALLERY MAGAZINE",
+        url: "https://www.tg-m.ru/catalog/en/picture/20753",
+      },
+      license: "Public Domain",
+    },
+  },
+  {
+    file: "les-orangers.png",
+    attribution: {
+      author: "Gustave Caillebotte",
+      source: {
+        label: "Wikimedia Commons",
+        url: "https://commons.wikimedia.org/wiki/File:Gustave_Caillebotte_-_The_Orange_Trees_-_Google_Art_Project.jpg",
+      },
+      license: "Public Domain",
+    },
+  },
+];
